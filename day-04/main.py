f1e3b6162cf1b9043f5f2cd8def053cb4f9f1653
Marek Balvín <marek267balvin@gmail.com>
2025-12-08 17:57:49 +0100

    Day 04 - part 1 complete

---
+++ day-04/main.py
@@ -0,0 +1,49 @@
+# 
+# Day 04 assignment: https://adventofcode.com/2025/day/4
+#
+
+def get_adjacent_rolls(x: int,y: int, grid: list[str]) -> int:
+    rolls = 0
+    adjacent = [
+        grid[y-1][x-1], grid[y-1][x], grid[y-1][x+1],
+        grid[y][x-1], grid[y][x+1],
+        grid[y+1][x-1], grid[y+1][x], grid[y+1][x+1],
+    ]
+    for cell in adjacent:
+        if cell == "@":
+            rolls += 1
+    return rolls
+
+def part_one_solution() -> int:
+    total_accessible = 0
+    lines = []
+    with open("./input.txt", "r") as input_file:
+        lines = [l.rstrip() for l in input_file.readlines()]
+        input_file.close()
+    Y = len(lines)
+    X = len(lines[0])
+    lines.insert(0, "." * X)
+    lines.append("." * X)
+    lines = ["." + line + "." for line in lines]
+    for y in range(1, Y+1):
+        for x in range(1, X+1):
+            if (lines[y][x] == "."):
+                continue
+            adj_rolls = get_adjacent_rolls(x,y,lines)
+            if (adj_rolls < 4):
+                total_accessible += 1
+    return total_accessible
+
+def part_two_solution() -> None:
+    pass
+
+
+def main() -> None:
+    print("============ Day 03 ============")
+    part_one_sol = part_one_solution()
+    print(f"Part 1 solution: {part_one_sol}")
+    part_two_sol = part_two_solution()
+    print(f"Part 2 solution: {part_two_sol}")
+
+if __name__ == "__main__":
+    main()
